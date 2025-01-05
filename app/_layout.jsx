25d3163a13915937a6b4cfcf0f3e45bb321da721
IgoDan <mr.igor.daniluk@gmail.com>
2025-01-05 15:02:48 +0100

    Added project base and tabs navigation

---
+++ app/_layout.jsx
@@ -0,0 +1,50 @@
+
+import React from 'react';
+import { Text, View } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import { Tabs } from "expo-router";
+
+export const unstable_settings = {
+  initialRouteName: "index",
+};
+
+export default function App() {
+  return (
+  <Tabs
+    screenOptions={({route}) => ({
+      tabBarIcon: ({focused, color, size}) => {
+        let iconName;
+        let rn = route.name;
+
+        if (rn === "index") {
+          iconName = focused ? 'home' : 'home-outline'
+        } else if (rn === "Movies") {
+          iconName = focused ? 'film' : 'film-outline'
+        } else if (rn === "Shows") {
+          iconName = focused ? "tv" : "tv-outline"
+        } else if (rn === "Profile") {
+          iconName = focused ? "person" : "person-outline"
+        }
+
+        return <Ionicons name={iconName} size={22} color="white"/>
+      },
+      headerShown: false,
+      tabBarStyle: { backgroundColor: '#1a202c' }, // Styl paska nawigacji
+      tabBarActiveTintColor: '#63b3ed', // Aktywny kolor
+      tabBarInactiveTintColor: '#a0aec0', // Nieaktywny kolor
+    })}>
+      <Tabs.Screen name='index' options={{
+        title: 'Home'
+      }} />
+      <Tabs.Screen name='Movies' options={{
+        title: 'Movies',
+      }} />
+      <Tabs.Screen name='Shows' options={{
+        title: 'Shows',
+      }} />
+      <Tabs.Screen name='Profile' options={{
+        title: 'Profile',
+      }} />
+    </Tabs>
+  );
+}
